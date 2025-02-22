1e2f4bd309ddcb13e33bca836d08004d650a0aab
IagoGrandal <i.gdelrio@udc.es>
2025-02-22 15:27:13 +0100

    imageUploader initialization 3 el retorno del rey

--- backend/src/main/com/inditex/uploadimage/UploadImageApplication.java
+++ backend/src/main/com/inditex/uploadimage/UploadImageApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UploadImageApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(UploadImageApplication.class, args);
     }
 }
+
